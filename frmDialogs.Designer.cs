c9b5a7f27e765d1cc06ad815c019848c87e00fc2
ahmedatefsyhh <ahmedatef10987@gmail.com>
2025-01-30 06:31:25 +0200

    Adding Dialogs

--- frmDialogs.Designer.cs
+++ frmDialogs.Designer.cs
@@ -28,12 +28,136 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button5 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button6 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // colorDialog1
+            // 
+            this.colorDialog1.FullOpen = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 49);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Change BackColor";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(194, 70);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(156, 49);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Change ForeColor";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 136);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(156, 49);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Change Font";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // fontDialog1
+            // 
+            this.fontDialog1.ShowApply = true;
+            this.fontDialog1.ShowColor = true;
+            this.fontDialog1.Apply += new System.EventHandler(this.fontDialog1_Apply);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Snap ITC", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(338, 50);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "This is a text";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(426, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(156, 49);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Save File Dialog";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(426, 82);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(156, 49);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Open File Dialog";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(426, 152);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(156, 49);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "folder Dialog";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // frmDialogs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "frmDialogs";
             this.Text = "frmDialogs";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
     }
 }
--- frmDialogs.cs
+++ frmDialogs.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace MyFirstWidowsFormsProject_PIZZA_
@@ -15,6 +10,119 @@ namespace MyFirstWidowsFormsProject_PIZZA_
         public frmDialogs()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+                textBox1.BackColor = colorDialog1.Color;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+                textBox1.ForeColor = colorDialog1.Color;
+        }
+
+        private Font OriginalFont;
+        private Color OriginalColor;
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OriginalColor = textBox1.ForeColor;
+            OriginalFont = textBox1.Font;
+
+            fontDialog1.Font = textBox1.Font;
+
+            if (fontDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Font = fontDialog1.Font;
+                textBox1.ForeColor = fontDialog1.Color;
+            }
+            else
+            {
+                textBox1.Font = OriginalFont;
+                textBox1.ForeColor = OriginalColor;
+            }
+
+        }
+
+        private void fontDialog1_Apply(object sender, EventArgs e)
+        {
+            textBox1.Font = fontDialog1.Font;
+            textBox1.ForeColor = fontDialog1.Color;
+        }
+
+        //private void button4_Click(object sender, EventArgs e)
+        //{
+        //    //saveFileDialog1.InitialDirectory = Application.StartupPath;
+        //    saveFileDialog1.InitialDirectory = @"C:\";
+
+        //    saveFileDialog1.Title = "this is dialog title";
+
+        //    saveFileDialog1.DefaultExt = "txt";
+
+        //    saveFileDialog1.Filter = "txt files (*.txt)|*.txt|All files(*.*)|*.*";
+
+        //    saveFileDialog1.FilterIndex = 2;
+
+        //    if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+        //    {
+        //        MessageBox.Show(saveFileDialog1.FileName);
+        //    }
+        //}
+
+        string GetFileName()
+        {
+            string[] FileDir = saveFileDialog1.FileName.Split('\\');
+            return FileDir[FileDir.Length - 1];
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.DefaultExt = "txt";
+            saveFileDialog1.Filter = "txt files (*.txt)|*.txt|all files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 2;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // next time he open the dialog the directory will be the same as last time
+                saveFileDialog1.InitialDirectory = saveFileDialog1.FileName;
+
+                StreamWriter File = new StreamWriter(saveFileDialog1.FileName);
+                File.Write(textBox1.Text);
+                File.Close();
+
+                // to get file name without full directory
+                saveFileDialog1.FileName = GetFileName();
+                MessageBox.Show(saveFileDialog1.FileName + " Saved Successfully", "Saved Successfully");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.DefaultExt = "txt";
+            openFileDialog1.Multiselect = true;
+            openFileDialog1.Filter = "txt files|*.txt|all files|*.*";
+            openFileDialog1.FilterIndex = 2;
+
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader File = new StreamReader(openFileDialog1.FileName);
+                textBox1.Text = File.ReadLine();
+                File.Close();
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show(folderBrowserDialog1.SelectedPath);
+            }
         }
     }
 }
